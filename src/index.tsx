f33c2632073b4690041e9ad977b7124d6e12a8d1
ValeriyKliuk <valeriy.kliuk@gmail.com>
2023-05-08 20:54:27 -0400

    initial ESLint setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './App'
 
 // Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
+document.body.innerHTML = '<div id="app"></div>'
 
 // Render your React component instead
-const root = createRoot(document.getElementById("app"));
-root.render(<App />);
+const root = createRoot(document.getElementById('app') as HTMLElement)
+root.render(<App />)
